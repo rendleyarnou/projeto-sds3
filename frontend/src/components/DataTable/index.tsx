d19f7923c7fa609983b9a99a282d8ab88ccc7ae6
Rendley Arnou <rendleyarnou7@gmail.com>
2021-05-09 09:34:36 -0300

    Routes

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -19,6 +19,62 @@ const DataTable = () => {
             <td>25</td>
             <td>15017.00</td>
           </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
         </tbody>
       </table>
     </div>
